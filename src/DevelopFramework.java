a2161a4779a8a28da5a60018aa4cc1e2ef259d19
Santhosh <Santhosh@gmail.com>
2025-01-11 21:44:05 +0530

    Framework development started

---
+++ src/DevelopFramework.java
@@ -0,0 +1,11 @@
+
+public class DevelopFramework {
+
+	public void pageObject() {
+		System.out.println("Develop some page objects");
+	}
+	
+	public void testngIntegration() {
+		System.out.println("Develop some assertions");
+	}
+}
