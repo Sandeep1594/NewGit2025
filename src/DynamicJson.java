93cb62e58e7b60256249068e7077327bd60fe418
Sam <sam@example.com>
2025-01-11 20:59:22 +0530

    Commit 3 by user 1

--- src/DynamicJson.java
+++ src/DynamicJson.java
@@ -4,6 +4,7 @@ public class DynamicJson {
 	public void gitLogic() {
 		System.out.println("Dummy commands");
 		System.out.println("Dummy commands added by User 2");
+		System.out.println("Dummy commands added by User 1");
 	}
 
 }
